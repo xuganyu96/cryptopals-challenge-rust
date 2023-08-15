979139ed03c076498b515cd4ff204ddfce81e5c2
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-08-14 19:16:34 -0700

    CHeckpoint

--- src/aes128ecb.rs
+++ src/aes128ecb.rs
@@ -1,25 +1,60 @@
 //! Convenient implementation of the AES-128-ECB stream cipher
+use std::error::Error;
+use aes::cipher::{generic_array::GenericArray, BlockCipher, BlockDecrypt, BlockEncrypt, KeyInit};
+use aes::Aes128;
 
 /// The cipher itself, which maintains state of the cipher. Electrnic code book are technically
 /// stateless, but for the sake of consistency with future implementation we will still use a
 /// struct to wrap around the backend implementation
-pub struct Aes128Ecb {}
+pub struct Aes128Ecb {
+    cipher: Aes128,
+}
 
 impl Aes128Ecb {
-    pub fn new() -> Self {
-        todo!();
-    }
+    /// Return the cipher with the keys copied from the input.
+    ///
+    /// If the key is not exactly 128-bit, the returned value will be an error
+    pub fn from_key(key: &[u8]) -> Result<Self, Box<dyn Error>> {
+        if key.len() != 16 {
+            return Err("Incorrect key length".into());
+        }
+        // unwrap is okay because of the length check
+        let cipher = Aes128::new_from_slice(key).unwrap();
 
-    pub fn set_key(&mut self, key: &[u8]) {
-        println!("Key set to {:?}", key);
+        return Ok(Self { cipher });
     }
 
     pub fn encrypt(&mut self, plaintext: &[u8]) -> Vec<u8> {
-        return plaintext.to_vec();
+        if plaintext.len() % 16 != 0 {
+            todo!("Padding is not yet implemented");
+        }
+        let mut output: Vec<u8> = vec![];
+
+        let nblocks = plaintext.len() / 16;
+        for i in 0..nblocks {
+            let mut block: [u8; 16] = [0u8; 16];
+            let block_start = i * 16;
+            let block_end = (i + 1) * 16;
+            block.copy_from_slice(plaintext.get(block_start..block_end).unwrap());
+            let mut block = GenericArray::from(block);
+            self.cipher.encrypt_block(&mut block);
+            let mut cipher_text = block.to_vec();
+            output.append(&mut cipher_text);
+        }
+        // TODO: before we implement proper padding, here is a hack: for plaintext whose length is
+        // a multiple of block sizes, a entire new block that consists entirely of padding needs to
+        // be appended
+        let block: [u8; 16] = [0x10u8; 16];
+        let mut block = GenericArray::from(block);
+        self.cipher.encrypt_block(&mut block);
+        let mut cipher_text = block.to_vec();
+        output.append(&mut cipher_text);
+
+        return output;
     }
 
-    pub fn decrypt(&mut self, ciphertext: &[u8]) -> Vec<u8> {
-        return ciphertext.to_vec();
+    pub fn decrypt(&mut self, ciphertext: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+        todo!();
     }
 }
 
@@ -29,14 +64,14 @@ mod tests {
 
     #[test]
     fn encrypt() {
-        let mut cipher = Aes128Ecb::new();
-        cipher.set_key(b"YELLOW SUBMARINE");
-        let plaintext = "Hello, world!";
+        let mut cipher = Aes128Ecb::from_key("0000000000000000".as_bytes()).unwrap();
+        let plaintext = "0000000000000000";
         let ciphertext = cipher.encrypt(plaintext.as_bytes());
+        println!("{:?}", ciphertext);
 
         assert_eq!(
             hex::encode(ciphertext),
-            "d1aa4f6578926542fbb6dd876cd2050860fa36707e45f499dba0f25b922301a5"
+            "f95c7f6b192b22bffefd1b779933fbfc346bce0b8eed34da10f6a8fabb844494"
         );
     }
 }
